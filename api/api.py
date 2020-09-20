0c2e061d4d907e34c740371fc7b8682f603081d5
Saie Kurakula <saiekurakula@gmail.com>
2020-09-20 09:40:03 -0400

    adding react hooks and api calls for artistsong

--- api/api.py
+++ api/api.py
@@ -1,13 +1,26 @@
 import time
 import requests
 import json
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
+@app.route('/artist')
+def get_artist():
+    artist = request.args['artist']
+    info = requests.get('http://theaudiodb.com/api/v1/json/1/search.php?s=' + artist)
+    return json.dumps(info.text)
 
-    info = requests.get('http://theaudiodb.com/api/v1/json/1/search.php?s=coldplay')
+@app.route('/song')
+def get_song():
+    song = request.args['song']
+    info = requests.get('http://ws.audioscrobbler.com/2.0/?method=track.search&track='+song+'&api_key=04c2af395292148ea292ff1fee738746&format=json')
+    return json.dumps(info.text)
+
+@app.route('/artistsong')
+def get_artist_song():
+    artist = request.args['artist']
+    song = request.args['song']
+    info = requests.get('http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=04c2af395292148ea292ff1fee738746&artist='+artist+'&track='+song+'&format=json')
     return json.dumps(info.text)
--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const [artist, setArtist] = useState('');
+  const [artistInfo, setArtistInfo] = useState('');
+  const [song, setSong] = useState('');
+  const [songInfo, setSongInfo] = useState('');
+  const [artistSongInfo, setArtistSongInfo] = useState('');
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data);
-      //setCurrentTime(data.time);
-    });
-  }, []);
 
+  var mySubmitHandler = (event) => {
+
+    event.preventDefault();
+
+    if ({artist}.artist !== '' && {song}.song !== '') {
+        fetch('/artistsong?artist='+{artist}.artist +'&song='+{song}.song).then(res => res.json()).then(data => {
+           setArtistSongInfo(data);
+        });
+    }
+    else if ({artist}.artist !== '' && {song}.song === '') {
+        fetch('/artist?artist=' + {artist}.artist).then(res => res.json()).then(data => {
+           setArtistInfo(data);
+        });
+    }
+    else if ({artist}.artist === '' && {song}.song !== '') {
+        fetch('/song?song=' + {song}.song).then(res => res.json()).then(data => {
+           setSongInfo(data);
+        });
+    }
+    else {
+    	alert("Please Enter an artist, a song, or both.");
+    }
+
+
+  }
+
+  var myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    if (nam==='artist'){
+	setArtist(val);
+    }
+    else if (nam === 'song') {
+	setSong(val);
+    }
+
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-	<p>The current time is {currentTime}.</p>
-      </header>
+      <form onSubmit={mySubmitHandler}>
+        <p>Enter Singer/Artist:</p>
+        <input name='artist' onChange={myChangeHandler} type="text" />
+        <p>And/Or Song Title:</p>
+        <input name='song' onChange={myChangeHandler} type="text" /><br /> <br />
+        <input type="submit" value="Submit" />
+      </form>
+	<div>
+	   <h1> Artist Info </h1>
+	   <p>{artistInfo}</p>
+        </div>
+	<div>
+	   <h1> Song Info </h1>
+	   <p>{songInfo}</p>
+        </div>
+	<div>
+	   <h1> Artist Song Info </h1>
+	   <p>{artistSongInfo}</p>
+        </div>
     </div>
   );
 }
